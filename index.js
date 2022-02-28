397dd780c03d3acc9101f08fc20019189bf581e0
Alexis <amart78@outlook.com>
2022-02-27 19:53:51 -0600

    commit tree ba0af2aa1deb3801ac5ef5ce535c44dcd1aaaabe

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+// create a new, empty array to hold the messages;
+// iterate through the input array and, inside the loop, build out the 'thank you' 
+// message for each name using string interpolation, 
+// then add that message to the new array you created;
+// after the loop finishes and all of the messages have been added to the new array, return the new array.
+
+function writeCards(names, event){
+    const cardMessage = [];
+
+    for (let i=0; i < names.length; i++){
+        let x = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+       cardMessage.push(x);         
+    }
+    
+    return cardMessage;
+}
+
+//(writeCards(["Guadalupe", "Ollie", "Aki"], 'birthday'));
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--
+    }
+}
+
+countDown(5);
